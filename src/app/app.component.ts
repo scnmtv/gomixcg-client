c4e2ae713256a7768697f72243fecfdd3f94d02a
Anže Jenšterle <anze.jensterle@castawaylabs.com>
2016-10-27 13:48:07 +0200

    fixed linting and added new features
    best changelog ever

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,50 @@
 import { Component } from '@angular/core';
 import * as io from "socket.io-client";
 @Component({
-  selector: 'go-cg',
-  templateUrl: 'app/app.component.pug'
+	selector: 'go-cg',
+	templateUrl: 'app/app.component.pug'
 })
 export class CGGoComponent {
-    socket: SocketIOClient.Socket;
-    socketUrl: string = 'http://localhost:8081';
-    socketConnected: boolean = false;
-    min: number = 0;
-    sec: number = 0;
-    msec: number = 0;
-    scoreHome: number = 0;
-    scoreAway: number = 0;
-    per = "1";
+		socket: SocketIOClient.Socket;
+		socketUrl: string = 'http://localhost:8081';
+		socketConnected: boolean = false;
+		min: number = 0;
+		sec: number = 0;
+		msec: number = 0;
+		scoreHome: number = 0;
+		scoreAway: number = 0;
+		per = "1";
 
-    constructor () {
-        this.connect()
-    }
+		constructor () {
+				this.connect()
+		}
 
-    connect () {
-        this.socket = io.connect(this.socketUrl);
-        this.socket.on('connect', function () {
-            this.socketConnected = true;
-        }.bind(this));
-        this.socket.on('disconnect', function () {
-            this.socketConnected = false;
-        }.bind(this));
-    }
+		connect () {
+				this.socket = io.connect(this.socketUrl);
+				this.socket.on('connect', function () {
+						this.socketConnected = true;
+				}.bind(this));
+				this.socket.on('disconnect', function () {
+						this.socketConnected = false;
+				}.bind(this));
+		}
 
-    setTime (): void {
-        this.socket.emit("config-change", "update -clock "+ this.min +" "+ this.sec + " " + this.msec + " " + this.per);
-    }
-    setScore (): void {
-        this.socket.emit("config-change", "update -score "+ this.scoreHome +" "+ this.scoreAway);
-    }
+		caspar (status: boolean): void {
+			this.socket.emit('config-change', 'caspar -' + (status ? 'on' : 'off'));
+		}
+
+		vmix (status: boolean): void {
+			this.socket.emit('config-change', 'vmix -' + (status ? 'on' : 'off'));
+		}
+
+		hdmi (status: boolean): void {
+			this.socket.emit('config-change', 'hdmi -' + (status ? 'on' : 'off'));
+		}
+
+		setTime (): void {
+			this.socket.emit('config-change', 'update -clock ' + this.min + ' ' + this.sec + ' ' + this.msec + ' ' + this.per);
+		}
+		setScore (): void {
+			this.socket.emit('config-change', 'update -score ' + this.scoreHome + ' ' + this.scoreAway);
+		}
 }
